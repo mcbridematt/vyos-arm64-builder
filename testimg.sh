79627b0f5c67b3ccd1009bf7f84b5e372c925161
Mathew McBride <matt@traverse.com.au>
2022-11-10 23:55:16 +0000

    testimg.sh: update for ISO

--- testimg.sh
+++ testimg.sh
@@ -2,10 +2,12 @@
 qemu-system-aarch64 -nographic \
     --enable-kvm \
     -cpu host -machine virt \
-    -bios QEMU_EFI.fd \
+    -bios /usr/share/qemu-efi-aarch64/QEMU_EFI.fd  \
     -smp 2 -m 1024 \
     -device virtio-rng-pci \
-    -hda VyOS.qcow2 \
+    -hda vyos-drive.qcow2 \
+    -cdrom vyos.iso \
     -netdev user,id=testlan -net nic,netdev=testlan \
     -netdev user,id=testwan -net nic,netdev=testwan
 
+#    -cdrom vyos.iso \
