cfccc7896a712f4af92c54fb1e19f600ce8ab840
Mathew McBride <matt@traverse.com.au>
2022-11-11 06:02:21 +0000

    build: do ISO build in container
    This is because we have our own versions of live-boot and other packages

--- build.sh
+++ build.sh
@@ -9,9 +9,6 @@ docker build -t "${CONTAINER_NAME}" vyos-build/docker/
 PKGBUILD_CONTAINER=$(docker create -it --privileged --entrypoint "/bin/bash" -v $(pwd):/tmp/vyos-build-arm64 "${CONTAINER_NAME}")
 docker start "${PKGBUILD_CONTAINER}"
 docker exec -i -t "${PKGBUILD_CONTAINER}" /bin/bash -c 'cd /tmp/vyos-build-arm64 && ./build-packages.sh'
+docker exec -i -t "${PKGBUILD_CONTAINER}" /bin/bash -c "cd /tmp/vyos-build-arm64/vyos-build && ./build-vyos-image --architecture=arm64 --build-by="${BUILD_BY}" --debug --build-type=development iso"
 docker stop "${PKGBUILD_CONTAINER}"
 docker rm "${PKGBUILD_CONTAINER}"
-
-cd vyos-build
-##export VYOS_BUILD_FLAVOUR=generic-arm64
-./build-vyos-image --architecture=arm64 --build-by="${BUILD_BY}" --debug --build-type=development iso
