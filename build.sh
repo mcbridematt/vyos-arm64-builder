7b65d03b9f8fe9d54779dab3a040b7941e56b915
Mathew McBride <matt@traverse.com.au>
2022-11-13 23:42:38 +0000

    build: disable IPv6 in build container (resolves issue with vyos-1x testsuite)
    See note on https://docs.vyos.io/en/equuleus/contributing/build-vyos.html

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ apt-get install -y kpartx make pbuilder devscripts python3-pystache python3-git 
 #CONTAINER_NAME="vyos/vyos-build:current-arm64"
 CONTAINER_NAME="vyos-arm64-libbpf"
 docker build -t "${CONTAINER_NAME}" vyos-build/docker/
-PKGBUILD_CONTAINER=$(docker create -it --privileged --entrypoint "/bin/bash" -v $(pwd):/tmp/vyos-build-arm64 "${CONTAINER_NAME}")
+PKGBUILD_CONTAINER=$(docker create -it --privileged --sysctl net.ipv6.conf.lo.disable_ipv6=0 --entrypoint "/bin/bash" -v $(pwd):/tmp/vyos-build-arm64 "${CONTAINER_NAME}")
 docker start "${PKGBUILD_CONTAINER}"
 docker exec -i -t "${PKGBUILD_CONTAINER}" /bin/bash -c 'cd /tmp/vyos-build-arm64 && ./build-packages.sh'
 docker exec -i -t "${PKGBUILD_CONTAINER}" /bin/bash -c "cd /tmp/vyos-build-arm64/vyos-build && ./build-vyos-image --architecture=arm64 --build-by="${BUILD_BY}" --debug --build-type=development iso"
