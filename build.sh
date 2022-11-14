4246266b30d4ab8780beef1f1cf8c10903cad0c7
Mathew McBride <matt@traverse.com.au>
2022-11-14 00:04:03 +0000

    build: change to build the ISO inside the container
    This is because we have our (VyOS) own versions of live-build and live-boot

--- build.sh
+++ build.sh
@@ -2,17 +2,13 @@
 set -e
 BUILD_BY="matt@traverse.com.au"
 
-apt-get install -y kpartx make live-build pbuilder devscripts python3-pystache python3-git python3-setuptools parted dosfstools
+apt-get install -y kpartx make pbuilder devscripts python3-pystache python3-git python3-setuptools parted dosfstools python3-toml python3-jinja2
 #CONTAINER_NAME="vyos/vyos-build:current-arm64"
 CONTAINER_NAME="vyos-arm64-libbpf"
 docker build -t "${CONTAINER_NAME}" vyos-build/docker/
-PKGBUILD_CONTAINER=$(docker create -it --privileged --entrypoint "/bin/bash" -v $(pwd):/tmp/vyos-build-arm64 "${CONTAINER_NAME}")
+PKGBUILD_CONTAINER=$(docker create -it --privileged --sysctl net.ipv6.conf.lo.disable_ipv6=0 --entrypoint "/bin/bash" -v $(pwd):/tmp/vyos-build-arm64 "${CONTAINER_NAME}")
 docker start "${PKGBUILD_CONTAINER}"
 docker exec -i -t "${PKGBUILD_CONTAINER}" /bin/bash -c 'cd /tmp/vyos-build-arm64 && ./build-packages.sh'
+docker exec -i -t "${PKGBUILD_CONTAINER}" /bin/bash -c "cd /tmp/vyos-build-arm64/vyos-build && ./build-vyos-image --architecture=arm64 --build-by="${BUILD_BY}" --debug --build-type=development iso"
 docker stop "${PKGBUILD_CONTAINER}"
 docker rm "${PKGBUILD_CONTAINER}"
-
-cd vyos-build
-export VYOS_BUILD_FLAVOUR=generic-arm64
-./configure --build-by="${BUILD_BY}" --architecture "arm64"
-make arm64
