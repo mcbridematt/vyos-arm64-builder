85c8e1821237ccb896bb3ac047069c6ad3350145
Mathew McBride <matt@traverse.com.au>
2023-12-22 12:14:59 +1100

    build: remove interactive/terminal options from docker exec
    This causes issues when run in GitLab CI

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ CONTAINER_NAME="vyos-build-arm64"
 docker build -t "${CONTAINER_NAME}" vyos-build/docker/
 PKGBUILD_CONTAINER=$(docker create -it --privileged --sysctl net.ipv6.conf.lo.disable_ipv6=0 --entrypoint "/bin/bash" -v $(pwd):/tmp/vyos-build-arm64 "${CONTAINER_NAME}")
 docker start "${PKGBUILD_CONTAINER}"
-docker exec -i -t "${PKGBUILD_CONTAINER}" /bin/bash -c 'cd /tmp/vyos-build-arm64 && ./build-packages.sh'
-docker exec -i -t "${PKGBUILD_CONTAINER}" /bin/bash -c "cd /tmp/vyos-build-arm64/vyos-build && ./build-vyos-image --architecture=arm64 --build-by="${BUILD_BY}" --debug --build-type=development iso"
+docker exec "${PKGBUILD_CONTAINER}" /bin/bash -c 'cd /tmp/vyos-build-arm64 && ./build-packages.sh'
+docker exec "${PKGBUILD_CONTAINER}" /bin/bash -c "cd /tmp/vyos-build-arm64/vyos-build && ./build-vyos-image --architecture=arm64 --build-by="${BUILD_BY}" --debug --build-type=development iso"
 docker stop "${PKGBUILD_CONTAINER}"
 docker rm "${PKGBUILD_CONTAINER}"
